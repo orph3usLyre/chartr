4b90652ab815ea2d1f367f404c5d147432d55c57
orph3uslyre <orph3uslyre@protonmail.com>
2024-10-04 09:02:30 +0200

    add update script for pub repo

---
+++ update_main_repo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp -r -f ./libbsb ../chartr_public/
+cp -r -f ./chartr ../chartr_public/
+cp ./README.md ../chartr_public/README.md
+cp ./Cargo* ../chartr_public/
+cp -r -f ./test_assets ../chartr_public/
+cp -r -f ./LICENSE ../chartr_public/LICENSE
+cp -r -f ./.gitignore ../chartr_public/.gitignore
+cp -r -f ./.cargo-rdme.toml ../chartr_public/.cargo-rdme.toml
